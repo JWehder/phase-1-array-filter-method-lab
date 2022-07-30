08ff5d1a499de91d4b1ca5fedb92a68c78eb6ab5
Jake Wehder <jw@email.comjake>
2022-07-30 11:51:52 -0500

    commit tree 5e23639121fe5e1d4b5623778a1dcdb72ac99670

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution here
+function findMatching(arr, name) {
+    const match = arr.filter(function(element) {
+        if (element.toLowerCase() === name.toLowerCase()) {
+            return element
+        }
+    })
+    return match;
+}
+
+function fuzzyMatch(arr, letters) {
+    const matchFirstTwoLetters = arr.filter(function(element) {
+        if (letters === element[0] + element[1]) {
+            return element
+        }
+    })
+    return matchFirstTwoLetters
+}
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(data, name) {
+    const findMatchingName = data.filter(function(element){
+        if (element.name === name) {
+            return element
+        }
+    })
+    return findMatchingName
+}
